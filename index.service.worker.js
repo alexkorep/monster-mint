37f7e4e3eb71b59140fdd8e41d40a6eb657b23f2
Alexey Korepanov <alexkorep@gmail.com>
2023-11-25 21:57:18 +0100

    Updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1700943963|4108484148";
+const CACHE_VERSION = "1700945832|819207717";
 const CACHE_PREFIX = "monster-mint-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
